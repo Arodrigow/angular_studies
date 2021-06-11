cc3563601715af44a6ed664b622e23989fbc4ad4
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-06-11 20:32:35 -0300

    angular example pt3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
